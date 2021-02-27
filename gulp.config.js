28d9f3423bf71b533f62cb14dec8d0e93d3d99be
sumanofficial <officialofsuman@gmail.com>
2021-02-27 22:52:19 +0530

    fix continuous loading js files

--- gulp.config.js
+++ gulp.config.js
@@ -11,15 +11,17 @@ export const dev_dir = "./templates";
 // ------------------------------------------
 
 // #StyleSheet
-export const dev_scss_dir = './templates/scss'
+export const dev_scss_dir = "./templates/scss";
 export const dev_scss_file = "./templates/scss/main.scss";
 export const prod_css_dir = "./templates/css";
 // ------------------------------------------
 
 // Javascript
-export const dev_js_dir = './templates/js'
-export const dev_js_file = "./templates/js/main.js";
+export const dev_js_dir = "./templates/js";
+// export const dev_js_file = ["./templates/js/*.js", "!./templates/js/*.min.js"];
+export const dev_js_file = ["./templates/js/*.js", "!./templates/js/*.min.js"];
 export const prod_js_dir = "./templates/js";
+// export const prod_js_dir = "./build/js";
 // ------------------------------------------
 
 // Images
@@ -29,4 +31,4 @@ export const img_types = "jpg,jpeg,png,svg,gif";
 // ------------------------------------------
 
 // Zip
-export const prod_zip_dir = './bundled'
+export const prod_zip_dir = "./bundled";
--- templates/js/main.js
+++ templates/js/main.js
@@ -1,2 +1,4 @@
 console.log("main js");
 console.log("testing");
+// console.log("object");
+console.log("object2");
